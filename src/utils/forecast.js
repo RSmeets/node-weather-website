227e47107f6363c12158248a97d3350633c37835
unknown <rudysmeets@hotmail.com>
2019-04-02 22:09:55 +0200

    Added extra info to forecast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/46628a9f6db8aab5ac12457164559780/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/46628a9f6db8aab5ac12457164559780/' + latitude + ',' + longitude +'?units=si'
 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The temperature high is ' + body.daily.data[0].temperatureHigh + ' degrees and low is ' + body.daily.data[0].temperatureLow + ' degrees' )
         }
     })
 }
