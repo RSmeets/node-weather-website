85f78fe2b7dba391b938673eb70522ad3fef4678
unknown <rudysmeets@hotmail.com>
2019-04-02 21:00:56 +0200

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('hi')
+
+
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+
+weatherform.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const location = search.value
+    messageOne.textContent = 'Loading Weather'
+    messageTwo.textContent = ''
+
+        fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.forecast
+                messageTwo.textContent = data.location
+            }
+        })
+    })
+})
